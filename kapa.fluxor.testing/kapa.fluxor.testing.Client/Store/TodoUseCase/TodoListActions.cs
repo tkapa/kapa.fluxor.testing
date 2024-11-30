0e5dd8f6665cc224cd91b04881f5547666de7045
Tylah Kapa <tylahkapa@gmail.com>
2024-11-30 12:07:13 +1000

    refactor: remove unused result action records in TodoListActions
    Eliminate `UpdateTodoListItemResultAction` and 
    `DeleteTodoListItemResultAction` records as they are no longer 
    needed. This simplifies the code and improves maintainability.

--- kapa.fluxor.testing/kapa.fluxor.testing.Client/Store/TodoUseCase/TodoListActions.cs
+++ kapa.fluxor.testing/kapa.fluxor.testing.Client/Store/TodoUseCase/TodoListActions.cs
@@ -6,7 +6,5 @@ public record CreateTodoListItemAction(string Title, int Order);
 public record CreateTodoListItemResultAction(TodoListItem Item);
 
 public record UpdateTodoListItemAction(TodoListItem UpdatedItem);
-public record UpdateTodoListItemResultAction(List<TodoListItem> Items);
 
 public record DeleteTodoListItemAction(Guid Id);
-public record DeleteTodoListItemResultAction(List<TodoListItem> Items);
